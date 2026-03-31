bf551220220d52dd4abd98c5ef55ac844ca3e45d
jeanpaulocorrea <jeanpaulocorrea@gmail.com>
2026-03-31 16:12:07 -0400

    feat: add ec2 auto scaling group and launch template

---
+++ server/data.ec2.ami.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "this" {
+  executable_users = ["self"]
+  most_recent      = true
+  owners           = ["136693071363"]
+
+  filter {
+    name   = "name"
+    values = ["debian-*"]
+  }
+    filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
--- server/variables.tf
+++ server/variables.tf
@@ -37,6 +37,38 @@ variable "ec2_resources" {
   
 }
 
+variable "control_plane_launch_template" {
+
+  type = object({
+    name = string
+    disable_api_stop = bool
+    disable_api_termination = bool
+    instance_type = string
+    instance_initiated_shutdown_behavior = string
+    ebs = object({
+      size = number
+      delete_on_termination = bool
+    })
+    
+  
+  })
+
+  default = {
+    name = "nsse-production-debian-control-plane-lt"
+    disable_api_stop = "true"
+    disable_api_termination = "true"
+    instance_type = "t3.micro"
+    instance_initiated_shutdown_behavior = "terminate"
+    ebs = {
+      size = 20
+      delete_on_termination = false
+    }
+  
+
+  }
+
+}
+
 variable "vpc_resources" {
   type = object({
     vpc = string
@@ -53,6 +85,35 @@ variable "tags" {
   default = {
     Environment = "Production"
     Project     = "NSSE"
+  }
+  
+}
+
+variable "control_plane_autoscaling_group" {
+  type = object({
+    name = string
+    max_size = number
+    min_size = number
+    desired_capacity = number
+    health_check_grace_period = number
+    health_check_type = string
+    instance_maintenance_policy = object({
+      min_healthy_percentage = number
+      max_healthy_percentage = number
+    })
+  })
+
+  default = {
+    name = "nsse-production-control-plane-asg"
+    max_size = 1
+    min_size = 1
+    desired_capacity = 1
+    health_check_grace_period = 180
+    health_check_type = "EC2"
+    instance_maintenance_policy = {
+      min_healthy_percentage = 100
+      max_healthy_percentage = 110
+    }
   }
   
 }
